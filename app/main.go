3113d8fefdf8a094a37b3ddd2c48285de3c2eb99
zxeenu <zeenu.lastorder@gmail.com>
2024-09-04 01:06:29 +0500

    Testing

--- app/main.go
+++ app/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/celestix/gotgproto/ext"
 	"github.com/celestix/gotgproto/sessionMaker"
 	"github.com/glebarez/sqlite"
+	"github.com/gotd/td/tg"
 )
 
 func main() {
@@ -55,9 +56,27 @@ func main() {
 
 }
 
+// Ref: https://github.com/celestix/gotgproto/blob/beta/examples/echo-bot/sqlite_session/main.go
 func echo(ctx *ext.Context, update *ext.Update) error {
 	msg := update.EffectiveMessage
 	text := msg.Message.Message
+	// userId := update.EffectiveMessage.ID
+	chatId := update.EffectiveChat().GetID()
+
+	var markupTable string = `
+	<pre>
+	| Tables   |      Are      |  Cool |
+	|----------|:-------------:|------:|
+	| col 1 is |  left-aligned | $1600 |
+	| col 2 is |    centered   |   $12 |
+	| col 3 is | right-aligned |    $1 |
+	</pre>
+	`
+
 	_, err := ctx.Reply(update, text, &ext.ReplyOpts{})
+	ctx.SendMessage(chatId, &tg.MessagesSendMessageRequest{
+		Message: markupTable,
+		// Peer: ... (No need of setting peer as we have passed chatId)
+	})
 	return err
 }
