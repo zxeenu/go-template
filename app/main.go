31d31e1093b5baba5a56e22dd509801732130442
Zxeenu <zeenu.lastorder@gmail.com>
2024-09-03 15:45:41 +0500

    Cleaning up

--- app/main.go
+++ app/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strconv"
 
 	"go.uber.org/zap"
 
 	"github.com/gotd/td/telegram"
-)
-
-const (
-	appID    = 0
-	appHash  = ""
-	appToken = ""
 )
 
 func main() {
@@ -24,6 +21,19 @@ func main() {
 	defer logger.Sync() // Flush any buffered log entries
 
 	logger.Sugar().Infoln("Bot Initialized...")
+
+	appIDStr := os.Getenv("TELEGRAM_APP_ID")
+	appHash := os.Getenv("TELEGRAM_HASH_ID")
+	appToken := os.Getenv("TELEGRAM_APP_TOKEN")
+
+	if appIDStr == "" || appHash == "" || appToken == "" {
+		logger.Sugar().Fatalln("Environment variables TELEGRAM_APP_ID or TELEGRAM_HASH_ID or TELEGRAM_APP_TOKEN are not set")
+	}
+
+	appID, appIDError := strconv.Atoi(appIDStr)
+	if appIDError != nil {
+		logger.Sugar().Fatalf("Error converting TELEGRAM_APP_ID to integer: %v", appIDError)
+	}
 
 	// https://core.telegram.org/api/obtaining_api_id
 	client := telegram.NewClient(appID, appHash, telegram.Options{
@@ -41,7 +51,8 @@ func main() {
 			return err
 		}
 
-		println(state)
+		// Printing the state properly
+		fmt.Printf("%+v\n", state)
 
 		// It is only valid to use client while this function is not returned
 		// and ctx is not cancelled.
@@ -53,8 +64,7 @@ func main() {
 		// Return to close client connection and free up resources.
 		return nil
 	}); err != nil {
-		println(err, "error")
-		panic(err)
+		logger.Sugar().Fatalln(err)
 	}
 	// Client is closed.
 }
