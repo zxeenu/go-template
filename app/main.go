899e69766941979441a5c8c452da7d1639967ec8
Zxeenu <zeenu.lastorder@gmail.com>
2024-09-10 11:57:54 +0500

    Testing GORM orm

--- app/main.go
+++ app/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"app/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-    fmt.Println("Hello, world!")
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.Product{})
+
+	// Create
+	db.Create(&models.Product{Code: "D42", Price: 100})
 }
---
+++ app/models/product.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
